fe0c06a79fdb5b57d96162cab73c4cd079ef6a3a
Yuras <uraspokhrel25@gmail.com>
2026-07-19 15:32:25 +0545

    Image URL added , PUT, Delete added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import PublicLayout from './components/Layout/PublicLayout'
 import HomePage from './components/Pages/HomePage'
 import AddUser from './components/Pages/AddUser'
+import User from './components/Pages/User'
+import { Toaster } from './components/ui/sonner'
 
 const App = () => {
   const router = createBrowserRouter([
@@ -18,11 +20,20 @@ const App = () => {
           path: '/add-user',
           element: <AddUser />,
         },
+        {
+          path: '/user',
+          element: <User />,
+        },
       ],
     },
   ])
 
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster position="top-right" />
+    </>
+  )
 }
 
 export default App
